d72c6d6353e65c3d4138e601ad3871fe1ba492bc
John Wiggins <lilwiggy123@gmail.com>
2018-01-12 22:05:28 -0600

    Fixed bug

--- coins.js
+++ coins.js
@@ -9,7 +9,22 @@ const {remote} = require('electron'),
 
 function checkCoins(price, head) {
     if (price > user1.coins) {
+        if(heads[`${head}`].purchased == 1) {
+            user1.head = head;
+            fs.writeFile(`./modules/user.json`, JSON.stringify(user1), function(err) {
+                if (err) {
+                    console.log("Sorry but something broke. Please send this message to @lilwiggy123 on twitter.\nSorry 'bout that.\n\n" + err)
+                }
+                remote.getCurrentWindow().loadURL(url.format({
+                    pathname: path.join(__dirname, 'main.html'),
+                    protocol: 'file',
+                    slashes: true
+                }));
+                alert(`${head} is now your grumphead. Please allow discord up to 15 seconds to make the changes.`)
+            })
+        } else {
         alert("Sorry looks like you need more Grump Coins for this. Get back to clickin'!")
+        }
     } else {
         if (heads[`${head}`].purchased == 0) {
             alert(`Thank you for purchasing the ${head} head! ${price} GrumpCoins have been reduced from your account. Please allow discord to take up to 15 seconds to add your grumphead.`)
@@ -31,7 +46,7 @@ function checkCoins(price, head) {
                     }));
                 })
             });
-        } else {
+        } else if(heads[`${head}`].purchased == 1) {
             user1.head = head;
             fs.writeFile(`./modules/user.json`, JSON.stringify(user1), function(err) {
                 if (err) {
